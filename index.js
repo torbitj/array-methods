68734cbad4de8c9a6b945c00420435b5b59f2957
JT <jstorbit@gmail.com>
2025-11-12 17:49:57 -0700

    Add get numbers bigger than 10 function and num array

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 const isAwesome = (names) => {
-  const awesomeNames = names.map((name) => `${name} is awesome!`)
+  const awesomeNames = names.map((name) => `${name} is awesome!`);
   return awesomeNames;
 }
 
 const namesArray = [`john`, `jim`, `janet`, `joseph`, `jesus`]
 
-console.log(isAwesome(namesArray))
+console.log(isAwesome(namesArray));
+
+const getBigNumbers = (numbers) => {
+  const bigNumbers = numbers.filter((number) => number > 10);
+  return bigNumbers;
+}
+
+const numArray = [1, 25, 10, 14, 5, 3, -6, 11, 8, 167, 9, 50, 2]
+
+console.log(getBigNumbers(numArray));
